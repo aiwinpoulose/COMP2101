b6c70283ba3f0e2e5102078efd40172f9d485faa
aiwinpoulose <you@yourdomain>
2020-01-29 15:54:37 -0800

    lab 2

--- bash/diceroller.sh
+++ bash/diceroller.sh
@@ -11,14 +11,5 @@ Rolling a six sided die and displaying the result.....
 
 $(( RANDOM % 6 + 1)) has been rolled
 "
-#Task : Modify the script to add rolling 5 six-sided dice
-echo "
-Rolling a five sided die and displaying result...
-$(( RANDOM % 6 + 1)), $(( RANDOM % 6 + 1)), $(( RANDOM % 6 + 1)), $(( RANDOM % 6 + 1)), $(( RANDOM % 6 + 1)) rolled "
 
-#Task : Modify the script to add rolling one 20-sided die
 
-echo "
-Rolling a 20 sided die and displaying result...
-$(( RANDOM % 20 + 1))
-"

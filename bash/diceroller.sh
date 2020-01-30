e4611daf37bd78eef2b46452fbb8e8bee7b1f43d
aiwinpoulose <you@yourdomain>
2020-01-29 16:09:09 -0800

    lab2

--- bash/diceroller.sh
+++ bash/diceroller.sh
@@ -10,6 +10,11 @@ echo "
 Rolling a six sided die and displaying the result.....
 
 $(( RANDOM % 6 + 1)) has been rolled
+
 "
+# Task: Improve this script by making it roll as many dice as there are letters in your first name, instead of just one
 
-
+#my first name : AIWIN
+#number of letters in my firstname : 5
+echo "We are rolling five six sided die
+$(( RANDOM % 6 + 1)), $(( RANDOM % 6 + 1)), $(( RANDOM % 6 + 1)), $(( RANDOM % 6 + 1)) and $(( RANDOM % 6 + 1)) have been rolled"

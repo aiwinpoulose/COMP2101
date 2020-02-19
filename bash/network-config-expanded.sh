3179d5b9c99f2f558a4746f8b635016696017dde
aiwinpoulose <you@yourdomain>
2020-02-19 06:53:04 -0800

    lab 4

--- bash/network-config-expanded.sh
+++ bash/network-config-expanded.sh
@@ -61,15 +61,21 @@ EOF
 # Per-interface report
 #####
 # define the interface being summarized
-interface="eno1"
 
+
+interfaces=$(ifconfig | grep -w -o '^[^ ][^ ]*:' | tr -d :)
+for interface in $interfaces; do
+  if [ $interface == "lo" ]
+  then
+   continue
+ fi
 # Find an address and hostname for the interface being summarized
 # we are assuming there is only one IPV4 address assigned to this interface
 ipv4_address=$(ip a s $interface|awk -F '[/ ]+' '/inet /{print $3}')
 ipv4_hostname=$(getent hosts $ipv4_address | awk '{print $2}')
 
 # Identify the network number for this interface and its name if it has one
-network_address=$(ip route list dev $interface scope link|cut -d ' ' -f 1)
+network_address=$(ip route list dev $interface scope link|cut -d ' ' -f 1 |grep -v '^169')
 network_number=$(cut -d / -f 1 <<<"$network_address")
 network_name=$(getent networks $network_number|awk '{print $1}')
 
@@ -80,8 +86,8 @@ Address         : $ipv4_address
 Name            : $ipv4_hostname
 Network Address : $network_address
 Network Name    : $network_name
-
 EOF
+done
 #####
 # End of per-interface report
 #####
